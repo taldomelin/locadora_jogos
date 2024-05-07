62b9e9292e80754131f1aa8f7b4e7f69d07de063
taldomelin <gumelo.cant@gmail.com>
2024-05-07 15:39:45 -0300

    Atualizando git

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,9 @@ use App\Models\Jogos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//* Jogos
 Route::post('cadastro/jogos', [JogosController::class, 'store']);
-
 Route::get('pesquisa/jogo/{id}', [JogosController::class, 'pesquisaPorId']);
-
 Route::post('buscaNome/jogo', [JogosController::class, 'pesquisarPorNomeJogo']);
-
 Route::get('retornarTodos/jogos', [JogosController::class, 'retornarTodosJogos']);
-
 Route::put('atualizar/jogos', [JogosController::class, 'atualizarJogos']);
-
 Route::delete('excluir/jogos/{id}', [JogosController::class, 'excluirJogos']);
