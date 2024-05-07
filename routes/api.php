87a0a4e5831e4c2b7e2610f531db1c5ce66f932a
taldomelin <gumelo.cant@gmail.com>
2024-05-07 15:39:19 -0300

    Atualizando git

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\JogosController;
+use App\Models\Jogos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//* Jogos
+Route::post('cadastro/jogos', [JogosController::class, 'store']);
+
+Route::get('pesquisa/jogo/{id}', [JogosController::class, 'pesquisaPorId']);
+
+Route::post('buscaNome/jogo', [JogosController::class, 'pesquisarPorNomeJogo']);
+
+Route::get('retornarTodos/jogos', [JogosController::class, 'retornarTodosJogos']);
+
+Route::put('atualizar/jogos', [JogosController::class, 'atualizarJogos']);
+
+Route::delete('excluir/jogos/{id}', [JogosController::class, 'excluirJogos']);
